a96504463385ce15547c9beac8f66c88b6013a09
Chioma5-star <chiomaosanebi5@gmail.com>
2026-08-18 14:53:12 +0100

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -28,8 +28,8 @@ app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://aidly-app.netlify.app", // update this after Netlify deploy
-    /\.netlify\.app$/              // allows any netlify subdomain
+    "https://aidlyy.netlify.app/", 
+    /\.netlify\.app$/              
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"]
